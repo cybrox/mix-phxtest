f335b08ef16a9cab196e4dce6113979f3d7fe8a5
Sven Gehring <cbrxde@gmail.com>
2018-05-16 11:32:42 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Phxtest.MixProject do
   def project do
     [
       app: :phxtest,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       build_embedded: false,
       start_permanent: false,
